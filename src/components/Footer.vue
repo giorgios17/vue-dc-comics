b0800d6079602a326cf5a501cdd9709d87bbf319
giorgios17 <giosur@live.it>
2022-04-06 17:07:03 +0200

    footer nav done

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -16,6 +16,51 @@
       />
       <p>DC POWER VISA</p>
     </section>
+    <section class="footernav">
+      <div class="col">
+        <ul>
+          <h3>DC COMICS</h3>
+          <li><a href="#">Characters</a></li>
+          <li><a href="#">Comics</a></li>
+          <li><a href="#">Movies</a></li>
+          <li><a href="#">TV</a></li>
+          <li><a href="#">Games</a></li>
+          <li><a href="#">Videos</a></li>
+          <li><a href="#">News</a></li>
+        </ul>
+        <ul>
+          <h3>SHOP</h3>
+          <li><a href="#">Shop DC</a></li>
+          <li><a href="#">Shop DC Collectibles</a></li>
+        </ul>
+      </div>
+      <div class="col">
+        <ul>
+          <h3>DC</h3>
+          <li><a href="#">Term Of Use</a></li>
+          <li><a href="#">Privacy policy (New)</a></li>
+          <li><a href="#">Ad Choices</a></li>
+          <li><a href="#">Advertising</a></li>
+          <li><a href="#">Jobs</a></li>
+          <li><a href="#">Subscriptions</a></li>
+          <li><a href="#">Talent Workshops</a></li>
+          <li><a href="#">CPSC Certificates</a></li>
+          <li><a href="#">Ratings</a></li>
+          <li><a href="#">Shop Help</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </div>
+      <div class="col">
+        <ul>
+          <h3>SITES</h3>
+          <li><a href="#">DC</a></li>
+          <li><a href="#">mad Magazine</a></li>
+          <li><a href="#">DC Kids</a></li>
+          <li><a href="#">DC Universe</a></li>
+          <li><a href="#">DC Power Visa</a></li>
+        </ul>
+      </div>
+    </section>
   </footer>
 </template>
 
@@ -38,6 +83,22 @@ export default {
   p {
     color: white;
     margin: 0px 80px 0px 10px;
+  }
+}
+.footernav {
+  background-image: url("../assets/footer-bg.jpg");
+  padding: 30px;
+  color: white;
+  display: flex;
+  ul {
+    list-style-type: none;
+    a {
+      text-decoration: none;
+      color: gray;
+      &:hover {
+        color: white;
+      }
+    }
   }
 }
 </style>
