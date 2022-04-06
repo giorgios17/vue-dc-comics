bdb8fb2c85e2a03f933c595d043557f61701d0f5
giorgios17 <giosur@live.it>
2022-04-06 18:34:03 +0200

    footer nav v-for

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -29,44 +29,31 @@
         <div class="col">
           <ul>
             <h3>DC COMICS</h3>
-            <li><a href="#">Characters</a></li>
-            <li><a href="#">Comics</a></li>
-            <li><a href="#">Movies</a></li>
-            <li><a href="#">TV</a></li>
-            <li><a href="#">Games</a></li>
-            <li><a href="#">Videos</a></li>
-            <li><a href="#">News</a></li>
+            <li :key="index" v-for="(link, index) in dcComics">
+              <a href="#">{{ link }}</a>
+            </li>
           </ul>
           <ul>
             <h3>SHOP</h3>
-            <li><a href="#">Shop DC</a></li>
-            <li><a href="#">Shop DC Collectibles</a></li>
+            <li :key="index" v-for="(link, index) in shop">
+              <a href="#">{{ link }}</a>
+            </li>
           </ul>
         </div>
         <div class="col">
           <ul>
             <h3>DC</h3>
-            <li><a href="#">Term Of Use</a></li>
-            <li><a href="#">Privacy policy (New)</a></li>
-            <li><a href="#">Ad Choices</a></li>
-            <li><a href="#">Advertising</a></li>
-            <li><a href="#">Jobs</a></li>
-            <li><a href="#">Subscriptions</a></li>
-            <li><a href="#">Talent Workshops</a></li>
-            <li><a href="#">CPSC Certificates</a></li>
-            <li><a href="#">Ratings</a></li>
-            <li><a href="#">Shop Help</a></li>
-            <li><a href="#">Contact Us</a></li>
+            <li :key="index" v-for="(link, index) in dc">
+              <a href="#">{{ link }}</a>
+            </li>
           </ul>
         </div>
         <div class="col">
           <ul>
             <h3>SITES</h3>
-            <li><a href="#">DC</a></li>
-            <li><a href="#">mad Magazine</a></li>
-            <li><a href="#">DC Kids</a></li>
-            <li><a href="#">DC Universe</a></li>
-            <li><a href="#">DC Power Visa</a></li>
+            <li :key="index" v-for="(link, index) in sites">
+              <a href="#">{{ link }}</a>
+            </li>
           </ul>
         </div>
       </div>
@@ -90,6 +77,34 @@
 <script>
 export default {
   name: "FooterComponent",
+  data: function () {
+    return {
+      dcComics: [
+        "Characters",
+        "Comics",
+        "Movies",
+        "TV",
+        "Games",
+        "Videos",
+        "News",
+      ],
+      shop: ["Shop DC", "Shop DC Collectibles"],
+      dc: [
+        "Terms of Use",
+        "Privacy Policy (New)",
+        "Ad Choices",
+        "Advertising",
+        "Jobs",
+        "Subscriptions",
+        "Talent Workshops",
+        "CPSC Certificates",
+        "Ratings",
+        "Shop Help",
+        "Contact Us",
+      ],
+      sites: ["DC", "MAD Magazine", "DC Kids", "DC Universe", "DC Power Visa"],
+    };
+  },
 };
 </script>
 
